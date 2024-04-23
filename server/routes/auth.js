effad5b71ceb1d3f7bbc54d28ba50390d7ab269f
iAbdullah80 <itsabdullah80@gmail.com>
2024-04-23 22:00:45 +0300

    update gitignore

---
+++ server/routes/auth.js
@@ -0,0 +1,40 @@
+const express=require('express')
+const router=express.Router()
+const passport=require('passport')
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+  }
+));
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+router.get('/google/callback', 
+  passport.authenticate('google', { 
+    failureRedirect: '/login-failure',
+    successRedirect: '/dashboard'
+}));
+
+router.get('login-failure', (req,res)=>{
+    res.send('Something went worng...')
+});
+
+// presist user data
+passport,passport.serializeUser(function(user, done){
+    done(null, user.id)
+})
+// Retrieve user data from session
+passport.deserializeUser(function(id, done){
+    User.FindById(id, function(err, user){
+        done(err, user)
+    })
+})
+module.exports=router
