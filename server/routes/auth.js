317aa2ff6be8fbbea5fda621ca4d1720a59bf3b5
iAbdullah80 <itsabdullah80@gmail.com>
2024-04-26 21:05:39 +0000

    google_auth_with_database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,14 +3,36 @@ const router=express.Router()
 const passport=require('passport')
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User')
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+  async function(accessToken, refreshToken, profile, done) {
+    const newUser = {
+      googleId: profile.id,
+      displayName: profile.displayName,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      profileImage: profile.photos[0].value
+    }
+
+    try {
+      let user = await User.findOne({googleId: profile.id})
+
+      if (user){
+        done(numll, user)
+      } else{
+        user = await User.create(newUser)
+        done(numll, user)
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 ));
 
@@ -28,7 +50,7 @@ router.get('login-failure', (req,res)=>{
 });
 
 // presist user data
-passport,passport.serializeUser(function(user, done){
+passport.serializeUser(function(user, done){
     done(null, user.id)
 })
 // Retrieve user data from session
